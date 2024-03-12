0ad5064a3bf500df11abac732e3524144776440e
Salah Ahmed <riddenariel@gmail.com>
2024-03-12 23:30:50 +0200

    add project skeleton

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,14 @@
+<template>
+	<h3>Add New Transaction</h3>
+	<form id="form">
+		<div class="form-control">
+			<label for="text">Text</label>
+			<input type="text" placeholder="Enter Text..." id="text" />
+		</div>
+		<div class="form-control">
+			<label for="amount">Amount</label>
+			<input type="number" id="amount" placeholder="Enter Amount..." />
+		</div>
+		<button class="btn">Add Transaction</button>
+	</form>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,12 @@
+<template>
+	<h3>History</h3>
+	<ul id="list" class="list">
+		<li class="minus">
+			Cash <span>-400 EGP</span> <button class="delete-btn">X</button>
+		</li>
+	</ul>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+</script>
